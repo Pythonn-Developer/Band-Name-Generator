dff24c34c4b53296ef3363c4b0183bc09783b0e7
Pythonn-Developer <87891464+Pythonn-Developer@users.noreply.github.com>
2021-07-24 12:28:31 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")                      # print the welcome greeting
+city = input("What's name of the city you grew up in?\n")         # take input of the user's city
+pet = input("What's your pet's name?\n")                          # take input of user's pet name
+print("Your band name could be " + city + " " + pet)              # display name as user's city petname
